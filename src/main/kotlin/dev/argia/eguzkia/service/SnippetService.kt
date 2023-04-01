a91648a320efcde02cfd451918810233e1827f3a
ARGI-BERRI <124055639+ARGI-BERRI@users.noreply.github.com>
2023-04-01 11:59:38 +0900

    chore: comment

--- src/main/kotlin/dev/argia/eguzkia/service/SnippetService.kt
+++ src/main/kotlin/dev/argia/eguzkia/service/SnippetService.kt
@@ -5,11 +5,26 @@ import org.springframework.stereotype.Service
 
 @Service
 interface SnippetService {
+    /**
+     * Gets a snippet of the given slug.
+     * If the slug has no content, this returns an empty content.
+     */
     fun get(slug: String): Snippet
 
+    /**
+     * Gets a list of slugs of available snippets.
+     * This may be empty.
+     */
     fun getAll(): List<Snippet>
 
+    /**
+     * Creates a new snippet.
+     * This generates a slug for the snippet automatically.
+     */
     fun create(snippet: Snippet): Snippet
 
+    /**
+     * Deletes a snippet of the given slug.
+     */
     fun delete(slug: String)
 }
